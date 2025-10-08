5a86474ededeb6a3b9ccd06df5b9e5d1eb93f9cb
SrZeAlex <83024037+SrZeAlex@users.noreply.github.com>
2025-10-08 16:06:01 +0100

    feat: add dataset and data_manipulation

--- data_manipulation.py
+++ data_manipulation.py
@@ -1,3 +1,19 @@
 import pandas as pd
 import csv
 
+dataset = pd.read_csv('bike_usage_2012.csv')
+#dataset 
+
+print('shape: ', dataset.shape)
+
+print('\ncolumns: ', dataset.columns)
+
+print('\ndata types: ', dataset.dtypes)
+
+print('\nMissing values:\n', dataset.isnull().sum())
+
+print('\n Duplicated rows: ', dataset.duplicated().sum())
+
+print('\nSummary statistics:\n', dataset.describe())
+
+print('\nTop 5 rows:\n', dataset.head())
